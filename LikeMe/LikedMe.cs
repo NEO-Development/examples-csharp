381da38a7cbf0823608b5a2eb518810a7cd00e59
Faruk Terzioğlu <faruk.terzioglu@hotmail.com>
2018-01-14 23:09:00 +0300

    like me progress

--- LikeMe/LikedMe.cs
+++ LikeMe/LikedMe.cs
@@ -51,21 +51,33 @@ namespace LikeMe
         private static string Name() => "LikedMe";
         private static string Symbol() => "LKM";
 
-        public static readonly int maxRate = 5;
-        public static readonly int rateRatio = 1;
+        public static readonly BigInteger maxRate = 5;
+        public static readonly BigInteger rateRatio = 1;
         public static readonly string initialRating = "3000";
 
-        private static readonly byte[] owner = { 134, 145, 12, 9, 163, 135, 211, 239, 30, 52, 183, 75, 246, 135, 108, 158, 9, 124, 124, 59 };
+        //031a6c6fbbdf02ca351745fa86b9ba5a9452d785ac4f7fc2b7548ca2a46c4fcf4a (owner)
+        //0226df9dee85d23f2159af39bbcb8551c3485884e00028e8f39dabe67443e4d615 (user a)
+        private static readonly byte[] owner =
+            new byte[] { 3, 26, 108, 111, 187, 223, 2, 202, 53, 23, 69, 250, 134, 185, 186, 90, 148, 82, 215, 133, 172, 79, 127, 194, 183, 84, 140, 162, 164, 108, 79, 207, 74 };
 
         public static object Main(string method, params object[] args)
         {
-            if (!Runtime.CheckWitness((byte[])args[0])) return false;
+            Runtime.Notify("Invoking : " + method);
 
-            if (method == "like") return Rate((byte[])args[0], (byte[])args[1], BytesToInt((byte[])args[2]), true);
-            if (method == "dislike") return Rate((byte[])args[0], (byte[])args[1], BytesToInt((byte[])args[2]), false);
+            //PUBLIC OPERATIONS  
             if (method == "rateOf") return RateOf((byte[])args[0]);
+
+            //ADMIN OPERATIONS
+            if (!Runtime.CheckWitness(owner)) { Runtime.Notify("Couldn't verify owner."); return false; }
+
+            //Like
+            if (method == "like")       return Rate((byte[])args[0], (byte[])args[1], BytesToInt((byte[])args[2]), true);
+            //Dislike
+            if (method == "dislike")    return Rate((byte[])args[0], (byte[])args[1], BytesToInt((byte[])args[2]), false);
+            //SetInitial
             if (method == "setInitial") return SetInitialRate((byte[])args[0]);
 
+            Runtime.Log("No operation is selected."); 
             return false;
         }
 
@@ -82,13 +94,7 @@ namespace LikeMe
             var buffer = new BigInteger(array);
             return buffer;
         }
-
-        /// <summary>
-        /// Rate calculation
-        /// </summary>
-        /// <param name="originatorRate"></param>
-        /// <param name="stars"></param>
-        /// <returns></returns>
+        
         private static BigInteger CalculateRate(BigInteger originatorRate, BigInteger stars)
         {
             return stars * rateRatio;
@@ -96,8 +102,23 @@ namespace LikeMe
 
         private static BigInteger AddRate(BigInteger originatorRate, BigInteger targetRate, BigInteger stars)
         {
-            BigInteger newRate = targetRate + CalculateRate(originatorRate, stars);
-            if (newRate > maxRate) newRate = maxRate;
+            Runtime.Notify("originatorRate", originatorRate);
+            Runtime.Notify("targetRate", targetRate);
+            Runtime.Notify("stars", stars);
+            
+            var temp2 = CalculateRate(originatorRate, stars);
+            Runtime.Notify("CalculateRate", temp2);
+
+            BigInteger newRate = targetRate + temp2;
+            Runtime.Notify("newRate", newRate);
+
+
+            if (newRate > maxRate)
+            {
+                Runtime.Notify("newRate > maxRate");
+                newRate = maxRate;
+            }
+
 
             return newRate;
         }
@@ -113,69 +134,80 @@ namespace LikeMe
 
         /// <summary>
         /// Get rate of someone
+        /// Operation name : rateOf
         /// </summary>
         /// <param name="address"></param>
         /// <returns></returns>
         public static BigInteger RateOf(byte[] address)
         {
             BigInteger currentBalance = Storage.Get(Storage.CurrentContext, address).AsBigInteger();
-            Runtime.Notify("Rate", address, currentBalance);
+            Runtime.Notify("RATE", address, currentBalance);
             return currentBalance;
         }
 
         /// <summary>
         /// Sets initial rate for account owner 'to', onlye SC owner can invoke 
+        /// Operation name : setInitial
         /// </summary>
         /// <param name="to"></param>
         /// <returns></returns>
-        private static bool SetInitialRate(byte[] to)
+        public static bool SetInitialRate(byte[] to)
         {
+            Runtime.Log("Setting initial rate for : " + to);
+
             //only owner can set initial 
-            if (!Runtime.CheckWitness(owner)) return false;
+            if (!Runtime.CheckWitness(owner)) { Runtime.Log("Couldn't verify owner."); return false; }
 
             //Check if already has rating 
             var currentRate = Storage.Get(Storage.CurrentContext, to);
-            if (currentRate != null) return false;
+            if (currentRate != null) { Runtime.Log("User already has rating : " + currentRate); return false; }
 
             //Put initial rating 
-            Runtime.Notify("PUT", to, initialRating);
             Storage.Put(Storage.CurrentContext, to, initialRating);
-            Runtime.Notify("Initial rate given.", to, initialRating, Blockchain.GetHeight());
+            Runtime.Log("Initial rate given : " + to);
+            Runtime.Log("Initial rating : " + initialRating);
 
             return true;
         }
 
         /// <summary>
-        /// Rate someone 
+        /// Someone rates someone, like or dislike 
+        /// Operation name : like, dislike
         /// </summary>
-        /// <param name="originator"></param>
-        /// <param name="to"></param>
-        /// <param name="stars"></param>
-        /// <param name="isLike"></param>
+        /// <param name="from">scripthash of rater</param>
+        /// <param name="to">scripthash of rated</param>
+        /// <param name="stars">amount of starts to rate</param>
+        /// <param name="isLike">is like or dislike</param>
         /// <returns></returns>
-        private static bool Rate(byte[] originator, byte[] to, BigInteger stars, bool isLike)
+        public static bool Rate(byte[] from, byte[] to, BigInteger stars, bool isLike)
         {
-            //Only originator can rate a user
-            if (!Runtime.CheckWitness(originator)) return false;
-
-            //Can not vote themselves
-            if (Runtime.CheckWitness(to)) return false;
-
+            if (stars <= 0) return false; 
+            
             //Check accounts 
-            var originatorRate = RateOf(originator);
+            var originatorRate = RateOf(from);
             var targetRate = RateOf(to);
 
-            if (originatorRate == null || targetRate == null) return false;
+            if (originatorRate == null || targetRate == null) {
+                Runtime.Notify("Null rate!", IntToBytes(originatorRate), IntToBytes(targetRate));
+                return false;
+            };
 
             //Set new rate 
-            BigInteger newTargetRate = originatorRate;
+            //BigInteger newTargetRate = targetRate;
 
-            if (isLike) newTargetRate = AddRate(originatorRate, targetRate, stars);
-            else newTargetRate = RemoveRate(originatorRate, targetRate, stars);
+            if (isLike)
+                Storage.Put(Storage.CurrentContext, from, targetRate + stars);
+            //newTargetRate = AddRate(originatorRate, targetRate, stars);
+            else
+                Storage.Put(Storage.CurrentContext, from, targetRate - stars);
+            //newTargetRate = RemoveRate(originatorRate, targetRate, stars);
 
             //Update rate 
-            Storage.Put(Storage.CurrentContext, to, newTargetRate.AsByteArray());
-            Runtime.Notify("Rate given.", originator, to, stars, isLike ? "Like" : "Dislike", Blockchain.GetHeight());
+            //Storage.Put(Storage.CurrentContext, to, newTargetRate);
+            //var newRate = BytesToInt(Storage.Get(Storage.CurrentContext, to));
+            //Runtime.Notify("New rate", IntToBytes(newRate));
+
+            RateOf(to);
 
             return true;
         }
